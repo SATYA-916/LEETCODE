b9ac629ce061b009c5d60c6e0fd0e9d76231887e
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-01 11:22:31 +0530

    Time: 1717 ms (25.57%), Space: 19.8 MB (74.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,7 +5,3 @@ class Solution:
                 if nums[i]+nums[j]==target:
                     print(nums[i],nums[j])
                     return [i,j]
-
-# Synced seamlessly with LeetHub Pro
-# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
