3ac3864657ad162c48496280929025c7aa246f0f
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-08 01:56:10 +0530

    Time: 0 ms (100%), Space: 19.4 MB (5.27%) - LeetHub - Time: O(?), Space: O(?)

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -3,3 +3,7 @@ from collections import Counter
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
         return len(set(Counter(s).values())) == 1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
