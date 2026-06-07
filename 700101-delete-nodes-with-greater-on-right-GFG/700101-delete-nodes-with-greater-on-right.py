38ce977e897176c99189e672379e45d165b795d4
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-08 01:33:44 +0530

    Added solution - LeetHub

---
+++ 700101-delete-nodes-with-greater-on-right-GFG/700101-delete-nodes-with-greater-on-right.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reverse(self, head):
+        prev = None
+        curr = head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        return prev
+
+    def compute(self, head):
+        head = self.reverse(head)
+
+        mx = head.data
+        curr = head
+
+        while curr and curr.next:
+            if curr.next.data < mx:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+                mx = curr.data
+
+        return self.reverse(head)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
