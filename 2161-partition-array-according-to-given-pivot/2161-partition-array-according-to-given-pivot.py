d908f97713d65437f45d1d0f801f1901d3411800
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-08 11:22:01 +0530

    Time: 26 ms (75.91%), Space: 33.7 MB (55.83%) - LeetHub - Time: O(?), Space: O(?)

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -13,3 +13,7 @@ class Solution:
                 greater.append(x)
 
         return less + equal + greater
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
