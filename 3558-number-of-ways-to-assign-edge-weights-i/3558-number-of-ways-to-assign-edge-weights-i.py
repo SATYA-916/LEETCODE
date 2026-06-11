e496aa18dbb0503cf73f106c63d3d33eec44e979
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-11 12:58:41 +0530

    Time: 304 ms (79.57%), Space: 113.5 MB (34.41%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
@@ -0,0 +1,25 @@
+class Solution:
+    MOD = 10**9 + 7
+
+    def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        n = len(edges) + 1
+
+        adj = [[] for _ in range(n + 1)]
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        max_depth = 0
+
+        def dfs(node: int, parent: int, depth: int) -> None:
+            nonlocal max_depth
+            max_depth = max(max_depth, depth)
+
+            for nei in adj[node]:
+                if nei != parent:
+                    dfs(nei, node, depth + 1)
+
+        dfs(1, 0, 0)
+
+        return pow(2, max_depth - 1, self.MOD)
