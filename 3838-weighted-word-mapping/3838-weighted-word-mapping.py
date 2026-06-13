2075eb881c06e445b7b86c15b2d5558d5d15fd4b
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-13 22:42:32 +0530

    Time: 14 ms (17.98%), Space: 19.3 MB (72.25%) - LeetHub - Time: O(n*m), Space: O(n)

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.py
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.py
@@ -1,19 +1,17 @@
 class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        l=[]
-        for i in range(ord('z'),ord('a')-1,-1):
-            l.append(chr(i))
-        temp,ans,j=0,"",0
+        j=0
+        r=""
+        temp=0
         for i in words:
             for k in i:
                 temp=temp+weights[ord(k)-ord('a')]
                 j=j+1
-            ans=ans+l[temp%26]
+            r=r+chr(abs(temp%26-26)+ord('a')-1)
             temp=0
-        return ans
-                
+        return r
 
-        
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
