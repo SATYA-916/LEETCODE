24a476cb51f3e9362738e3064e6580cc020335d6
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-17 09:56:18 +0530

    Time: 1 ms (41.37%), Space: 19.4 MB (11%) - LeetHub - Time: O(n), Space: O(n)

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -5,3 +5,8 @@ class Solution:
         for i in ans.split()[::-1]:
             x=x+" "+i
         return x[1:]
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
