f3c2661ef418ad48936dee60b2268144521ba07c
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-17 11:44:25 +0530

    Time: 0 ms (100%), Space: 19.3 MB (92.09%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -0,0 +1,19 @@
+from fractions import Fraction
+class Solution:
+    def fractionAddition(self, ex: str) -> str:
+        x=eval(ex)
+        if x==int(x):
+            return str(int(x))+"/1"
+        return str(Fraction(x).limit_denominator())
+        # s=""
+        # if x<=1:
+        #     if x==int(x):
+        #         return str(int(x))+"/1"
+        #     if x!=0:
+        #         x=1/x
+        #     if x>0:
+        #         s="1/"+str(math.ceil(x))
+        #         return s
+        #     else:
+        #         s="-1/"+str(abs(math.ceil(x)-1))
+        #         return s
