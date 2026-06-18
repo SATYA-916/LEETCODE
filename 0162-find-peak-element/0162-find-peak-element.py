1cb829c0f4eb36bf3c7c5829f89ac5e310d72e72
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-18 09:10:03 +0530

    Time: 0 ms (100%), Space: 19.3 MB (78.96%) - LeetHub - Time: O(n), Space: O(1)

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -2,3 +2,7 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         return nums.index(max(nums))
         
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
