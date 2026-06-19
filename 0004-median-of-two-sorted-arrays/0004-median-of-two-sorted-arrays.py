3e8949c4de1615af70fc709506eef9a3e4abbee5
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-19 13:52:08 +0530

    Time: 3 ms (47.25%), Space: 19.5 MB (77.07%) - LeetHub - Time: O((m+n)log(m+n)), Space: O(m+n)

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -6,3 +6,7 @@ class Solution:
         if len(l1)%2==0:
             return (l1[len(l1)//2]+l1[(len(l1)//2)-1])/2
         return l1[len(l1)//2]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
