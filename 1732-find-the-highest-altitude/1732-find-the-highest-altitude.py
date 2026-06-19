3c63167e7252dff8881d49948208bb73895fe7bf
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-19 09:15:44 +0530

    Time: 0 ms (100%), Space: 19.1 MB (97.8%) - LeetHub - Time: O(n), Space: O(1)

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -6,3 +6,7 @@ class Solution:
             ans=ans+gain[i]
             c=max(c,ans)
         return max(c,0)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
