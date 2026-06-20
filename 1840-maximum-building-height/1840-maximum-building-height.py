a5d300e3715d4d62d7f466f379574916240f1fcb
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-20 10:37:40 +0530

    Time: 329 ms (6.82%), Space: 48.1 MB (9.09%) - LeetHub - Time: O(n), Space: O(n)

--- 1840-maximum-building-height/1840-maximum-building-height.py
+++ 1840-maximum-building-height/1840-maximum-building-height.py
@@ -37,3 +37,7 @@ class Solution:
             ans = max(ans, (h1 + h2 + d) // 2)
 
         return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
