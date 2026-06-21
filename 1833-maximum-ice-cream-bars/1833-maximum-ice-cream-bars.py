4b5f04f2ceee7207c778a73b28f72afb2af91df7
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-21 08:54:58 +0530

    Time: 51 ms (92.25%), Space: 30.8 MB (97.05%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,15 +1,19 @@
 class Solution:
-    def maxIceCream(self, costs: List[int], coins: int) -> int:
-        costs.sort()
-        for i in range(len(costs)):
-            coins=coins-costs[i]
-            if coins<0:
-                return i
-        return len(costs)
+    def maxIceCream(self, cost: List[int], coin: int) -> int:
+        ans,pre=[0]*(max(cost)+1),0
+        for i in cost:
+            ans[i]=ans[i]+1
+        i=0
+        while(i <len(ans)):
+            if ans[i]!=0:
+                coin=coin-(i)
+                if coin<0:
+                    return pre
+                pre,ans[i]=pre+1,ans[i]-1
+            else:
+                i+=1
+        return len(cost)
+
             
 
         
-
-# Synced seamlessly with LeetHub Pro
-# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
