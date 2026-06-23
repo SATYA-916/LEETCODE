e78c94f3fea934150eeb1ccde73b43c539613321
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-23 14:02:38 +0530

    Time: 138 ms (11.8%), Space: 20 MB (24.39%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nu=[False]*len(nums)
+        res=set()
+        def help(ans,nu):
+            if len(ans)==len(nums):
+                res.add(tuple(ans))
+                return
+            for i in range(len(nums)):
+                if not nu[i]:
+                    nu[i]=True
+                    help(ans+[nums[i]],nu)
+                    nu[i]=False
+            return res
+        x=help([],nu)
+        ans=[]
+        for i in x:
+            ans.append(list(i))
+        return ans
+
+                    
+        
