1be400a5c1934dc3ce43cef52fdc6941633185e7
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-25 13:43:40 +0530

    Time: 214 ms (81.33%), Space: 19.7 MB (9.33%) - LeetHub - Time: O(n log n), Space: O(n)

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
@@ -61,3 +61,7 @@ class Solution:
             st.update(1, 0, len(vals) - 1, idx)
 
         return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
