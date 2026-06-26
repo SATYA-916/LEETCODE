6b40015ee28f1c2748381f2281c25a4d7ca6e0fe
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-26 09:41:42 +0530

    Time: 27 ms (64.41%), Space: 33.2 MB (62.71%) - LeetHub - Time: O(n), Space: O(1)

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.py
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.py
@@ -10,3 +10,7 @@ class Solution:
                 return len(nums)-1
         return 0
         
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
