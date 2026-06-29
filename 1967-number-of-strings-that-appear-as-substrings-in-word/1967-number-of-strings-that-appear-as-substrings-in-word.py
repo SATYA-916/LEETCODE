b34a21367a594fae055c99b6435f95cb9911ec55
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-29 12:45:39 +0530

    Time: 0 ms (100%), Space: 19.4 MB (6.22%) - LeetHub - Time: O(n*m), Space: O(1)

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
@@ -5,3 +5,8 @@ class Solution:
             if i in w:
                 c+=1
         return c
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
