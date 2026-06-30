b027d4820eda6471284b72acdfcf8f3f4bfa4f59
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-06-30 21:55:42 +0530

    Time: 99 ms (64.31%), Space: 19.4 MB (85.27%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        cnt = {'a':0, 'b':0, 'c':0}
+        l = 0
+        ans = 0
+        n = len(s)
+
+        for r in range(n):
+            cnt[s[r]] += 1
+
+            while cnt['a'] > 0 and cnt['b'] > 0 and cnt['c'] > 0:
+                ans += n - r
+                cnt[s[l]] -= 1
+                l += 1
+
+        return ans
