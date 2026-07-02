7ba50d4c32c8460e56666bcd6d4beb3f9b3716f7
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-07-02 16:23:05 +0530

    Time: 1532 ms (5.19%), Space: 22.1 MB (21.7%) - LeetHub - Time: O(?), Space: O(?)

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.py
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.py
@@ -30,3 +30,7 @@ class Solution:
             return ans
 
         return help(0, 0, h) >= 1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
