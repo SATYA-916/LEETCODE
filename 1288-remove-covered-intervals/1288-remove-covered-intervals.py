69f9dd05a03e4d7817460edd2317bfc82275107c
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-07-06 17:40:38 +0530

    Time: 23 ms (6.43%), Space: 19.6 MB (80.88%) - LeetHub - Time: O(?), Space: O(?)

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -8,3 +8,7 @@ class Solution:
                         c+=1 
                         break
         return len(inter)-c  
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
