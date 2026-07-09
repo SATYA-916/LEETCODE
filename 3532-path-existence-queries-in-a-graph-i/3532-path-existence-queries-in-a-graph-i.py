c5e6ebea39f193d5c400ba8f41bcb2c1334c43ce
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-07-09 15:48:37 +0530

    Time: 97 ms (59.52%), Space: 49.4 MB (92.06%) - LeetHub - Time: O(n + q), Space: O(n)

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -9,3 +9,7 @@ class Solution:
                 comp[i] = comp[i - 1] + 1
 
         return [comp[u] == comp[v] for u, v in queries]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
