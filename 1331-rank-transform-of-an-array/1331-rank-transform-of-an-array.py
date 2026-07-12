8b79eefe0c484c15bf629e0dd8b7ca71872c19e2
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-07-12 09:18:54 +0530

    Time: 55 ms (29.3%), Space: 37.5 MB (60.61%) - LeetHub - Time: O(n log n), Space: O(n)

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -8,3 +8,7 @@ class Solution:
         for i in range(len(arr)):
             arr[i]=d[arr[i]]
         return arr
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
