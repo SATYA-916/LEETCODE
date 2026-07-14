a12f55e6148a7a7bce322eac7e2a0eb228fdabc1
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-07-14 10:41:12 +0530

    Time: 3773 ms (13.64%), Space: 589.6 MB (34.09%) - LeetHub

---
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.py
@@ -0,0 +1,24 @@
+from math import gcd
+from functools import cache
+from typing import List
+
+class Solution:
+    def subsequencePairCount(self, nums: List[int]) -> int:
+        MOD = 10**9 + 7
+
+        @cache
+        def dp(i, g1, g2):
+            if i == len(nums):
+                return 1 if g1 == g2 and g1 != 0 else 0
+
+            ans = dp(i + 1, g1, g2)
+
+            ng1 = nums[i] if g1 == 0 else gcd(g1, nums[i])
+            ans += dp(i + 1, ng1, g2)
+
+            ng2 = nums[i] if g2 == 0 else gcd(g2, nums[i])
+            ans += dp(i + 1, g1, ng2)
+
+            return ans % MOD
+
+        return dp(0, 0, 0)
