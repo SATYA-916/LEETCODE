88029109975089ff8589bdebf9f614a9e7b248f0
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-07-26 16:36:04 +0530

    Time: 3 ms (72.46%), Space: 20.6 MB (5.1%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordBreak(self, s: str, w: List[str]) -> bool:
+        @cache
+        def help(x):
+            if x == s:
+                return True
+
+            if not s.startswith(x):
+                return False
+
+            for word in w:
+                if help(x + word):
+                    return True
+
+            return False
+
+        return help("")
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
