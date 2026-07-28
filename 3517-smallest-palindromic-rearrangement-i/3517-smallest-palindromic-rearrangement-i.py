b9817a5b49a6605a8ce11b0e3f8cd163d68008df
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-07-28 18:36:32 +0530

    Time: 327 ms (30.42%), Space: 20.8 MB (65.44%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -1,21 +1,25 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
-        print(chr(97),ord('a'))
-        a,ans,k,st=[0]*26,"",0,''
-        for i in s:
-            a[ord(i)-ord('a')]+=1
-        for i in range(26):
-            if a[i]%2==1:
-                st=chr(ord('a')+i)
-                a[i]-=1
-        for i in range(26):
-            a[i]/=2
-        for i in range(26):
-            if a[i]>0:
-                ans=ans+(chr(ord('a')+i))*int(a[i])
-                a[i]=0
-        return ans+st+ans[::-1]
+        a = [0] * 26
+        ans = ""
+        st = ""
 
+        for ch in s:
+            a[ord(ch) - ord('a')] += 1
+
+        for i in range(26):
+            if a[i] % 2 == 1:
+                st = chr(ord('a') + i)
+                a[i] -= 1
+
+        for i in range(26):
+            a[i] //= 2
+
+        for i in range(26):
+            if a[i] > 0:
+                ans += chr(ord('a') + i) * a[i]
+
+        return ans + st + ans[::-1]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
