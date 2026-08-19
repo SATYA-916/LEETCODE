9e61c843b959e5802f24c57226df04b9162c832d
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-08-19 15:07:54 +0530

    Time: 113 ms (8.48%), Space: 22.3 MB (97.43%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxNumberOfFamilies(self, n: int, r: List[List[int]]) -> int:
+        r.sort()
+        l = [0] * 11
+        m = r[0][0]
+        i = 0
+        ans = 2 * (m - 1)
+        while i < len(r):
+            while i < len(r) and r[i][0] == m:  # same row
+                l[r[i][1]] = 1
+                i += 1
+            if sum(l[2:6]) == 0 and sum(l[6:10]) == 0:
+                ans += 2
+            elif sum(l[2:6]) == 0 or sum(l[6:10]) == 0 or sum(l[4:8]) == 0:
+                ans += 1
+            if i == len(r):
+                break
+            ans += (r[i][0] - m - 1) * 2
+            m = r[i][0]
+            l = [0] * 11
+        return ans+(n-r[-1][0])*2
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
