c94efd3f81903609b4b812b86b7a492f53d1902d
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-09-13 13:32:58 +0530

    Time: 1159 ms (12.43%), Space: 19.4 MB (89.95%) - LeetHub - Time: O(n^4), Space: O(1)

--- 0835-image-overlap/0835-image-overlap.py
+++ 0835-image-overlap/0835-image-overlap.py
@@ -1,25 +1,23 @@
 class Solution:
     def largestOverlap(self, m1: List[List[int]], m2: List[List[int]]) -> int:
-
-        def search(m1, m2, x, y):
-            s = 0
-
-            for i in range(len(m1)):
-                for j in range(len(m1[0])):
-                    if 0 <= i + x < len(m2) and 0 <= j + y < len(m2[0]):
-                        if m1[i][j] == 1 and m2[i + x][j + y] == 1:
-                            s += 1
-
+        if len(m1)==1 and len(m1[0])==1 and len(m2)==1 and len(m2[0])==1:
+            return m1[0][0] and m2[0][0]
+        def search(m1,m2,x,y):
+            s=0
+            n=len(m1)
+            for i in range(len(m2)):
+                for j in range(len(m2[0])):
+                    if 0<=i+x<n and 0<=j+y<n and m1[i][j]+m2[i+x][j+y]==2:
+                        s+=1
             return s
-
-        n = len(m1)
-        m = 0
-
-        for i in range(-n + 1, n):
-            for j in range(-n + 1, n):
-                m = max(m, search(m1, m2, i, j))
-
+        m=0
+        n=len(m1)
+        for i in range(-n+1,len(m1)):
+                for j in range(-n+1,len(m1[0])):
+                    m=max(m,search(m1,m2,i,j))
         return m
+
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
