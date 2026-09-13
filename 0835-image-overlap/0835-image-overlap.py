491c7fdc8e7627991b71ae51aef1373665675f9d
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-09-13 14:13:10 +0530

    Time: 193 ms (75.4%), Space: 19.9 MB (28.84%) - LeetHub - Time: O(n^4), Space: O(n^2)

--- 0835-image-overlap/0835-image-overlap.py
+++ 0835-image-overlap/0835-image-overlap.py
@@ -1,23 +1,28 @@
+from collections import defaultdict
+
 class Solution:
-    def largestOverlap(self, m1: List[List[int]], m2: List[List[int]]) -> int:
-        if len(m1)==1 and len(m1[0])==1 and len(m2)==1 and len(m2[0])==1:
-            return m1[0][0] and m2[0][0]
-        def search(m1,m2,x,y):
-            s=0
-            n=len(m1)
-            for i in range(len(m2)):
-                for j in range(len(m2[0])):
-                    if 0<=i+x<n and 0<=j+y<n and m1[i][j]+m2[i+x][j+y]==2:
-                        s+=1
-            return s
-        m=0
-        n=len(m1)
-        for i in range(-n+1,len(m1)):
-                for j in range(-n+1,len(m1[0])):
-                    m=max(m,search(m1,m2,i,j))
-        return m
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
 
+        n = len(img2)
 
+        one = []
+        on = []
+
+        for i in range(n):
+            for j in range(n):
+                if img2[i][j] == 1:
+                    one.append((i, j))
+
+                if img1[i][j] == 1:
+                    on.append((i, j))
+
+        d = defaultdict(int)
+
+        for i in one:
+            for j in on:
+                d[(j[0] - i[0], j[1] - i[1])] += 1
+
+        return max(d.values(), default=0)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
