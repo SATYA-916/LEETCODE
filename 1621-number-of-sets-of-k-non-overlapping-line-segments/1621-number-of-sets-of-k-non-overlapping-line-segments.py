30f112fab685c266e6caa65858ed3c374b69684a
K. Satya Sampath Kumar <satyasampath79@gmail.com>
2026-09-16 19:01:45 +0530

    Time: 825 ms (38.79%), Space: 42.7 MB (45.69%) - LeetHub - Time: O(n * k), Space: O(n * k)

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
@@ -1,23 +1,41 @@
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
-        MOD = 10**9 + 7
 
-        @cache
-        def solve(a, x):
-            if x == 0:
-                return 1
-            if a == n - 1:
-                return 0
+        m = 10**9 + 7
 
-            return (solve(a + 1, x) + suffix(a + 1, x - 1)) % MOD
+        dp = [[0] * (k + 1) for _ in range(n + 1)]
 
-        @cache
-        def suffix(a, x):
-            if a == n:
-                return 0
-            return (solve(a, x) + suffix(a + 1, x)) % MOD
+        for i in range(n + 1):
 
-        return solve(0, k)
+            dp[i][0] = 1
+
+            # if k==0:
+            #     return 1
+
+            # if i>=n:
+            #     return 0
+
+        for K in range(1, k + 1):
+
+            suffix = [0] * (n + 1)
+
+            for i in range(n - 1, -1, -1):
+                suffix[i] = (suffix[i + 1] + dp[i][K - 1]) % m
+
+            for i in range(n - 1, -1, -1):
+
+                # skip=solve(i+1,k)
+                skip = dp[i + 1][K]
+
+                # take=0
+                # for j in range(i+1,n):
+                #     take+=solve(j,k-1)
+
+                take = suffix[i + 1]
+
+                dp[i][K] = (take + skip) % m
+
+        return dp[0][k] % m
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
